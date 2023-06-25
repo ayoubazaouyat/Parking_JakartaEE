f45b58153c0a91521b85f202829b6a0c56d1002d
assia <assia@bekk.com>
2023-06-25 12:26:30 +0200

    Nachzahlen korrigieren

--- src/main/java/com/example/parhausprj/Nachzahlen.java
+++ src/main/java/com/example/parhausprj/Nachzahlen.java
@@ -6,25 +6,26 @@ import java.time.LocalDateTime;
 public class Nachzahlen  extends State {
 
     @Override
-    public State bezahle(Ticket t) {
-        LocalDateTime date = myLocalDate.myCurrentTime();
-        double zwischenzeit = Duration.between( t.getBezahlzeit(),date).toHours() + ((double)((Duration.between(t.getBezahlzeit(),date)).toMinutes())%60.)/60.0;
-        t.setPrice(t.getTicketPrice() * zwischenzeit);
-        t.setBezahlzeit(date);
-        return new TicketBezahlt() ;
-
+    public State verlasse(Ticket t) throws IllegalStateException {
+        throw new IllegalStateException("Sie müssen zuerst die zusätzlichen Gebühren bezahlen ");
     }
 
     @Override
     public State verliere(Ticket t) {
-        t.setVerlustGeb(t.getVerlustGeb()+30.0);
+        t.setVerlustGeb(t.getVerlustGeb() + 30.0);
         return new TicketVerloren();
     }
 
     @Override
-    public State verlasse(Ticket t) {
+    public State bezahle(Ticket t) {
+        LocalDateTime date = myLocalDate.myCurrentTime();
+        double neuezeit = Duration.between( t.getBezahlzeit(),date).toHours() + ((double)((Duration.between(t.getBezahlzeit(),date)).toMinutes())%60.)/60.0;
+        t.setPrice(t.getTicketPrice() * neuezeit);
+        t.setBezahlzeit(date);
 
-        throw new IllegalStateException("You have to pay the extra fees first");
+        return new TicketBezahlt() ;
+
     }
+
 
 }
