a33979b84522e9df358080b7f6f67da7411bd3f3
Ismail Amrati <ismail.amrati@smail.inf.h-brs.de>
2023-06-29 12:46:55 +0200

    fix jenkins failure

--- src/test/java/FeedbackTableGeneratorTest.java
+++ src/test/java/FeedbackTableGeneratorTest.java
@@ -15,9 +15,9 @@ public class FeedbackTableGeneratorTest {
         feedbacks.add(new Feedback("Jane Smith", "janesmith@example.com", "Another complaint message", "2023-06-26 13:00:00", "complaint"));
         feedbacks.add(new Feedback("Mike Johnson", "mikejohnson@example.com", "Yet another complaint", "2023-06-26 14:00:00", "feedback"));
 
-        String expectedTable = "<table><tr><th>Name</th><th>Email</th><th>Nachricht</th></tr>" +
-                "<tr><td>John Doe</td><td>johndoe@example.com</td><td>This is a complaint message</td></tr>" +
-                "<tr><td>Jane Smith</td><td>janesmith@example.com</td><td>Another complaint message</td></tr>" +
+        String expectedTable = "<table><tr><th>Name</th><th>Email</th><th>Time</th><th>Nachricht</th></tr>" +
+                "<tr><td>John Doe</td><td>johndoe@example.com</td><td>2023-06-26 12:00:00</td><td>This is a complaint message</td></tr>" +
+                "<tr><td>Jane Smith</td><td>janesmith@example.com</td><td>2023-06-26 13:00:00</td><td>Another complaint message</td></tr>" +
                 "</table>";
 
         String actualTable = FeedbackTableGenerator.generateComplaintTable(feedbacks);
@@ -32,9 +32,9 @@ public class FeedbackTableGeneratorTest {
         feedbacks.add(new Feedback("Jane Smith", "janesmith@example.com", "Another feedback message", "2023-06-26 13:00:00", "complaint"));
         feedbacks.add(new Feedback("Mike Johnson", "mikejohnson@example.com", "Yet another feedback", "2023-06-26 14:00:00", "feedback"));
 
-        String expectedTable = "<table><tr><th>Name</th><th>Email</th><th>Nachricht</th></tr>" +
-                "<tr><td>John Doe</td><td>johndoe@example.com</td><td>This is a feedback message</td></tr>" +
-                "<tr><td>Mike Johnson</td><td>mikejohnson@example.com</td><td>Yet another feedback</td></tr>" +
+        String expectedTable = "<table><tr><th>Name</th><th>Email</th><th>Time</th><th>Nachricht</th></tr>" +
+                "<tr><td>John Doe</td><td>johndoe@example.com</td><td>2023-06-26 12:00:00</td><td>This is a feedback message</td></tr>" +
+                "<tr><td>Mike Johnson</td><td>mikejohnson@example.com</td><td>2023-06-26 14:00:00</td><td>Yet another feedback</td></tr>" +
                 "</table>";
 
         String actualTable = FeedbackTableGenerator.generateFeedbackTable(feedbacks);
